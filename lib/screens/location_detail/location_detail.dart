40dc72733ae06343eb723bfd181d92948cb60553
yen <phongsathon.yen@gmail.com>
2019-11-24 16:24:29 +0700

    first commit

---
+++ lib/screens/location_detail/location_detail.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LocationDetail extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Location-Detail'),
+      ),
+      body: Text('data'),
+    );
+  }
+}
